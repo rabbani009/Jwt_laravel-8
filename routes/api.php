2a2d632b7b94283e44feeff4500426f48c3d67dd
Your Name <alaminatekur111@yahoo.com>
2022-08-04 11:06:02 +0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\ControllerRegister;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register',[ControllerRegister::class,'register'])->name('register');
+Route::post('login',[ControllerRegister::class,'login'])->name('login');
+Route::get('logout',[ControllerRegister::class,'logout'])->name('logout');
